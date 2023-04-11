7524dfdb6cbb87f03b37b5eedf41890c526d69b6
JessePires <jessepires2010@gmail.com>
2023-04-10 22:43:14 -0300

    feat: Create method GETFILESLIST in server and start implementation in client.

--- tcp/src/ex2/TCPClient.java
+++ tcp/src/ex2/TCPClient.java
@@ -12,16 +12,12 @@ import java.nio.ByteOrder;
 import java.io.*;
 import java.util.Scanner;
 
-
-
-
 public class TCPClient {
-	public static final String ANSI_GREEN = "\u001B[32m";
-	public static final String ANSI_RESET = "\u001B[0m";
-	public static String user = "user";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static String user = "user";
 
-	
-	public static ByteBuffer createHeader(byte messageType, byte commandId, byte filenameSize, String filename) {
+    public static ByteBuffer createHeader(byte messageType, byte commandId, byte filenameSize, String filename) {
         ByteBuffer header = ByteBuffer.allocate(259); // tamanho do cabeçalho de solicitação
         header.order(ByteOrder.BIG_ENDIAN);
 
@@ -30,19 +26,23 @@ public class TCPClient {
         header.put(2, filenameSize);
         header.position(3);
         header.put(filename.getBytes());
-        
-        return header;
-	}
 
-    public static void printResponseStatusCode(byte commandId, byte statusCode){
-        switch (commandId){
+        return header;
+    }
+
+    public static void printResponseStatusCode(byte commandId, byte statusCode) {
+        switch (commandId) {
             case 1: // ADDFILE
-                if(statusCode == 2) System.out.println("Erro ao tentar criar arquivo :(");
-                else System.out.println("Arquivo criado com sucesso :)");
+                if (statusCode == 2)
+                    System.out.println("Erro ao tentar criar arquivo :(");
+                else
+                    System.out.println("Arquivo criado com sucesso :)");
                 break;
             case 2: // DELETE
-                if(statusCode == 2) System.out.println("Erro ao tentar deletar arquivo :(");
-                else System.out.println("Arquivo deletar com sucesso :)");
+                if (statusCode == 2)
+                    System.out.println("Erro ao tentar deletar arquivo :(");
+                else
+                    System.out.println("Arquivo deletar com sucesso :)");
                 break;
             case 3: // GETFILESLIST
                 break;
@@ -50,146 +50,151 @@ public class TCPClient {
                 break;
         }
     }
-	
-	public static void main (String args[]) {
-	    Socket clientSocket = null; // socket do cliente
+
+    public static void main(String args[]) {
+        Socket clientSocket = null; // socket do cliente
         Scanner reader = new Scanner(System.in);
-            
-            try{
-                /* Endereço e porta do servidor */
-                int serverPort = 6666;   
-                InetAddress serverAddr = InetAddress.getByName("127.0.0.1");
-                
-                /* conecta com o servidor */  
-                clientSocket = new Socket(serverAddr, serverPort);  
-                
-                /* cria objetos de leitura e escrita */
-                DataInputStream in = new DataInputStream(clientSocket.getInputStream());
-                DataOutputStream out = new DataOutputStream(clientSocket.getOutputStream());
-            
-                /* protocolo de comunicação */
-                String keyboardBuffer = "";
-                byte[] bytes = null;
-                
-                loop:while (true) {
-                    System.out.print(ANSI_GREEN+user+"$ "+ANSI_RESET);
-                    keyboardBuffer = reader.nextLine(); // lê mensagem via teclado
-                    String[] argCommand = keyboardBuffer.split(" ");
-                    
-                    
-                    String command = argCommand[0];
-                    String filename = "";
-                    byte messageType = 1;
-                    byte commandId = 0;
 
-                    switch(command){
-                        case "PARAR":
-                            break loop;
-                        case "ADDFILE":
-                            filename = argCommand[1];
-                            commandId = 1;                            
-                            break;
-                        case "DELETE":
-                            filename = argCommand[1];    
-                            commandId = 2;
-                            break;
-                        case "GETFILELIST":
-                            commandId = 3;
-                            break;
-                        case "GETFILE":
-                            filename = argCommand[1];    
-                            commandId = 4;
-                            break;
-                        default:
-                            System.out.println("Comando desconhecido");
-                            continue;
-                    }
+        try {
+            /* Endereço e porta do servidor */
+            int serverPort = 6666;
+            InetAddress serverAddr = InetAddress.getByName("127.0.0.1");
 
-                    byte filenameSize = (byte) filename.length();
-                    ByteBuffer buffer = createHeader(messageType, commandId, filenameSize, filename);
+            /* conecta com o servidor */
+            clientSocket = new Socket(serverAddr, serverPort);
 
-                    bytes = buffer.array();
-                    int size = buffer.limit();
-                    out.write(bytes, 0, size);      	// envia o header para o servidor
-                    out.flush();
-                    
+            /* cria objetos de leitura e escrita */
+            DataInputStream in = new DataInputStream(clientSocket.getInputStream());
+            DataOutputStream out = new DataOutputStream(clientSocket.getOutputStream());
 
-                    // ***********************************
-                    // Aguardando resposta do servidor
-                    // ***********************************
-                    in.read(bytes);    
-                    
-                    buffer = ByteBuffer.wrap(bytes);
-                    buffer.order(ByteOrder.BIG_ENDIAN);
-                    byte responseMessageType = buffer.get(0);
-                    byte responseCommandId = buffer.get(1);
-                    byte responseStatusCode = buffer.get(2);
+            /* protocolo de comunicação */
+            String keyboardBuffer = "";
+            byte[] bytes = null;
 
-                    
-                    // ***********************************
-                    // Aguardando resposta do conteudo dos arquivos
-                    // ***********************************
-                    int sizeOfContent = 0;
-                    switch(command){
-                        case "GETFILELIST":
+            loop: while (true) {
+                System.out.print(ANSI_GREEN + user + "$ " + ANSI_RESET);
+                keyboardBuffer = reader.nextLine(); // lê mensagem via teclado
+                String[] argCommand = keyboardBuffer.split(" ");
+
+                String command = argCommand[0];
+                String filename = "";
+                byte messageType = 1;
+                byte commandId = 0;
+
+                switch (command) {
+                    case "PARAR":
+                        break loop;
+                    case "ADDFILE":
+                        filename = argCommand[1];
+                        commandId = 1;
+                        break;
+                    case "DELETE":
+                        filename = argCommand[1];
+                        commandId = 2;
+                        break;
+                    case "GETFILELIST":
+                        commandId = 3;
+                        break;
+                    case "GETFILE":
+                        filename = argCommand[1];
+                        commandId = 4;
+                        break;
+                    default:
+                        System.out.println("Comando desconhecido");
+                        continue;
+                }
+
+                byte filenameSize = (byte) filename.length();
+                ByteBuffer buffer = createHeader(messageType, commandId, filenameSize, filename);
+
+                bytes = buffer.array();
+                int size = buffer.limit();
+                out.write(bytes, 0, size); // envia o header para o servidor
+                out.flush();
+
+                // ***********************************
+                // Aguardando resposta do servidor
+                // ***********************************
+                in.read(bytes);
+
+                buffer = ByteBuffer.wrap(bytes);
+                buffer.order(ByteOrder.BIG_ENDIAN);
+                byte responseMessageType = buffer.get(0);
+                byte responseCommandId = buffer.get(1);
+                byte responseStatusCode = buffer.get(2);
+
+                // ***********************************
+                // Aguardando resposta do conteudo dos arquivos
+                // ***********************************
+                int sizeOfContent = 0;
+                switch (command) {
+                    case "GETFILELIST":
+                        in.read(bytes);
+                        buffer = ByteBuffer.wrap(bytes);
+                        buffer.order(ByteOrder.BIG_ENDIAN);
+                        sizeOfContent = buffer.getInt();
+
+                        bytes = new byte[1];
+                        byte[] filenamesContentByte = new byte[sizeOfContent];
+
+                        for (int i = 0; i < sizeOfContent; i++) {
                             in.read(bytes);
-                            buffer = ByteBuffer.wrap(bytes);
-                            buffer.order(ByteOrder.BIG_ENDIAN);
-                            sizeOfContent = buffer.getInt();
-                            break;
-                        case "GETFILE":
+                            byte b = bytes[0];
+                            filenamesContentByte[i] = b;
+                        }
+
+                        break;
+                    case "GETFILE":
+                        in.read(bytes);
+                        buffer = ByteBuffer.wrap(bytes);
+                        buffer.order(ByteOrder.BIG_ENDIAN);
+                        sizeOfContent = buffer.getInt();
+
+                        System.out.println("sizeOfContent: " + sizeOfContent);
+
+                        bytes = new byte[1];
+                        byte[] contentByte = new byte[sizeOfContent];
+                        for (int i = 0; i < sizeOfContent; i++) {
                             in.read(bytes);
-                            buffer = ByteBuffer.wrap(bytes);
-                            buffer.order(ByteOrder.BIG_ENDIAN);
-                            sizeOfContent = buffer.getInt();
+                            byte b = bytes[0];
+                            contentByte[i] = b;
+                        }
 
-                            System.out.println("sizeOfContent: "+sizeOfContent);
-                            
-                            bytes = new byte[1];
-                            byte[] contentByte = new byte[sizeOfContent];
-                            for(int i = 0; i < sizeOfContent; i++){
-                                in.read(bytes);
-                                byte b = bytes[0];
-                                contentByte[i] = b;
-                            }
+                        String content = new String(contentByte);
+                        System.out.println(content);
+                        break;
+                    default:
+                        printResponseStatusCode(responseCommandId, responseStatusCode);
+                        break;
+                }
 
-                            String content = new String(contentByte);
-                            System.out.println(content);
-                            break;
-                        default:
-                            printResponseStatusCode(responseCommandId, responseStatusCode);
-                            break;
-                    }   
+                // boolean isNumber = buffer.matches("[0-9]+");
 
-                    
-                    
-                    // boolean isNumber = buffer.matches("[0-9]+");
-                    
-                    // de acordo com a especificação da atividade alguns comandos retornam a quantidade de mensagens que irao enviar em seguida
-                    // if(isNumber) {
-                    // 	int amountOfMessages = Integer.parseInt(buffer);
-                    // 	while(amountOfMessages > 0) {
-                    // 		buffer = in.readUTF();
-                    // 		System.out.println(buffer);
-                    // 		amountOfMessages--;
-                    // 	}
-                    	
-                    // }else {
-                    // 	System.out.println(buffer);                    	
-                    // }    
-                } 
+                // de acordo com a especificação da atividade alguns comandos retornam a
+                // quantidade de mensagens que irao enviar em seguida
+                // if(isNumber) {
+                // int amountOfMessages = Integer.parseInt(buffer);
+                // while(amountOfMessages > 0) {
+                // buffer = in.readUTF();
+                // System.out.println(buffer);
+                // amountOfMessages--;
+                // }
 
-	    }catch (Exception ue){
+                // }else {
+                // System.out.println(buffer);
+                // }
+            }
+
+        } catch (Exception ue) {
             System.out.println("Socket:" + ue.getMessage());
-        } 
+        }
         // finally {
-        //     reader.close();
-        //     try {
-        //         clientSocket.close();
-        //     } catch (IOException ioe) {
-        //         System.out.println("IO: " + ioe);;
-        //     }
+        // reader.close();
+        // try {
+        // clientSocket.close();
+        // } catch (IOException ioe) {
+        // System.out.println("IO: " + ioe);;
         // }
-     }
+        // }
+    }
 }
-
--- tcp/src/ex2/TCPServer.java
+++ tcp/src/ex2/TCPServer.java
@@ -7,232 +7,281 @@ package ex2;
 import java.net.*;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import java.io.*;
 
 public class TCPServer {
 
-    public static void main(String args[]) {
-        try {
-            int serverPort = 6666; // porta do servidor
+	public static void main(String args[]) {
+		try {
+			int serverPort = 6666; // porta do servidor
 
-            
-            
-            /* cria um socket e mapeia a porta para aguardar conexao */
-            ServerSocket listenSocket = new ServerSocket(serverPort);
+			/* cria um socket e mapeia a porta para aguardar conexao */
+			ServerSocket listenSocket = new ServerSocket(serverPort);
 
-            while (true) {
-                System.out.println("Servidor aguardando conexao ...");
+			while (true) {
+				System.out.println("Servidor aguardando conexao ...");
 
-                /* aguarda conexoes */
-                Socket clientSocket = listenSocket.accept();
+				/* aguarda conexoes */
+				Socket clientSocket = listenSocket.accept();
 
-                System.out.println("Cliente conectado ... Criando thread ...");
+				System.out.println("Cliente conectado ... Criando thread ...");
 
-                /* cria um thread para atender a conexao */
-                ClientThread c = new ClientThread(clientSocket);
+				/* cria um thread para atender a conexao */
+				ClientThread c = new ClientThread(clientSocket);
 
-                /* inicializa a thread */
-                c.start();
-            } //while
+				/* inicializa a thread */
+				c.start();
+			} // while
 
-
-        } catch (IOException e) {
-            System.out.println("Listen socket:" + e.getMessage());
-        } //catch
-    } //main
-} //class
+		} catch (IOException e) {
+			System.out.println("Listen socket:" + e.getMessage());
+		} // catch
+	} // main
+} // class
 
 /**
  * Classe ClientThread: Thread responsavel pela comunicacao
- * Descricao: Rebebe um socket, cria os objetos de leitura e escrita,
+ * Descricao: Recebe um socket, cria os objetos de leitura e escrita,
  * aguarda msgs clientes e responde com a msg + :OK
  */
 class ClientThread extends Thread {
 	private static final byte SUCCESS = 1;
 	private static final byte ERROR = 2;
 
-    DataInputStream in;
-    DataOutputStream out;
-    Socket clientSocket;
-    
-    String currentPath;
+	DataInputStream in;
+	DataOutputStream out;
+	Socket clientSocket;
 
-    Auth authenticator = new Auth();
-    public boolean isConnected = false;
-    
-    public ClientThread(Socket clientSocket) {
-        try {
-            this.clientSocket = clientSocket;
-            in = new DataInputStream(clientSocket.getInputStream());
-            out = new DataOutputStream(clientSocket.getOutputStream());
-            
-            File theDir = new File("./src/ex2/jhonatan");
-						if (!theDir.exists()){
-								theDir.mkdirs();
-						}
-            
-        		// aqui devemos concatenar o nome da pasta do usuario
-            this.currentPath = System.getProperty("user.dir")+"/src/ex2/jhonatan";
-        } catch (IOException ioe) {
-            System.out.println("Connection:" + ioe.getMessage());
-        } //catch
-    } //construtor
-    
+	String currentPath;
 
+	Auth authenticator = new Auth();
+	public boolean isConnected = false;
 
-		public ByteBuffer createResponseHeader(byte messageType, byte commandId, byte statusCode) {
-			ByteBuffer header = ByteBuffer.allocate(3);
-			header.order(ByteOrder.BIG_ENDIAN);
+	public ClientThread(Socket clientSocket) {
+		try {
+			this.clientSocket = clientSocket;
+			in = new DataInputStream(clientSocket.getInputStream());
+			out = new DataOutputStream(clientSocket.getOutputStream());
 
-			header.put(0, messageType);
-			header.put(1, commandId);
-			header.put(2, statusCode);
-			
-			return header;
-		}
+			File theDir = new File("./src/ex2/jhonatan");
+			if (!theDir.exists()) {
+				theDir.mkdirs();
+			}
 
-		public byte addFile(String filename, String content) throws IOException{
-			String path = this.currentPath+"/"+filename;
-			File file = new File(path);
+			// aqui devemos concatenar o nome da pasta do usuario
+			this.currentPath = System.getProperty("user.dir") + "/src/ex2/jhonatan";
+		} catch (IOException ioe) {
+			System.out.println("Connection:" + ioe.getMessage());
+		} // catch
+	} // construtor
 
-			if (file.createNewFile()) {
-				 FileWriter writer = new FileWriter(path, true);
-				 BufferedWriter buffer = new BufferedWriter(writer);
-				 buffer.write(content);
-				 buffer.flush();
-				 buffer.close();
-				return SUCCESS;
-			} else {
-				return ERROR;
-			}	
-		}
+	public ByteBuffer createResponseHeader(byte messageType, byte commandId, byte statusCode) {
+		ByteBuffer header = ByteBuffer.allocate(3);
+		header.order(ByteOrder.BIG_ENDIAN);
 
-		public byte removeFile(String filename){
-			String path = this.currentPath+"/"+filename;
-			File file = new File(path);
+		header.put(0, messageType);
+		header.put(1, commandId);
+		header.put(2, statusCode);
 
-			if (file.exists()) {
-					if (file.delete()) {
-							return SUCCESS;
-					} 
-			} 
-				
+		return header;
+	}
+
+	public byte addFile(String filename, String content) throws IOException {
+		String path = this.currentPath + "/" + filename;
+		File file = new File(path);
+
+		if (file.createNewFile()) {
+			FileWriter writer = new FileWriter(path, true);
+			BufferedWriter buffer = new BufferedWriter(writer);
+			buffer.write(content);
+			buffer.flush();
+			buffer.close();
+			return SUCCESS;
+		} else {
 			return ERROR;
 		}
+	}
 
+	public byte removeFile(String filename) {
+		String path = this.currentPath + "/" + filename;
+		File file = new File(path);
 
-		public byte[] getFile(String filename){
-			byte[] response = null;
-			try {
-				String path = this.currentPath+"/"+filename;
-				File file = new File(path);
-
-				FileInputStream inputStream = new FileInputStream(file);
-				response = inputStream.readAllBytes();
-				inputStream.close();
-
-				return response;
-			} catch (Exception e) {
-				return response;
+		if (file.exists()) {
+			if (file.delete()) {
+				return SUCCESS;
 			}
 		}
 
-    /* metodo executado ao iniciar a thread - start() */
-    @Override
-    public void run() {
-			try {        	 
+		return ERROR;
+	}
 
-				System.out.println("args: " + this.currentPath);
+	public List<String> getFilesList(String directory) {
+		File dir = new File(directory);
+		File[] arrayFiles = dir.listFiles();
 
-				int headerSize = 259;
-				
-				while (true) {
-					byte[] bytes = new byte[headerSize];
-					this.in.read(bytes);
-					ByteBuffer header = ByteBuffer.wrap(bytes);
-					header.order(ByteOrder.BIG_ENDIAN);
+		// create a new ArrayList
+		List<String> fileList = new ArrayList<String>();
 
-					// ***********************************
-					// Obtendo cabeçalho de solicitação
-					// ***********************************
-					byte messageType = header.get(0);
-					byte commandId = header.get(1);
-					byte filenameSize = header.get(2);
-					byte[] byteFilename = Arrays.copyOfRange(bytes, 3, filenameSize+3);
-					String filename = new String(byteFilename);
-				
+		for (File f : arrayFiles) {
+			if (f.isFile()) {
+				fileList.add(f.getName());
+			}
+		}
 
-					byte statusCode = ERROR;
-					byte[] responseContent = null;
+		return fileList;
+	}
 
-					switch(commandId){
-						case 1: // ADDFILE
-								statusCode = addFile(filename, "conteudo");
-								break;
-						case 2: // DELETE
-								statusCode = removeFile(filename);
-								break;
-						case 3: // GETFILESLIST
-								break;
-						case 4: // GETFILE
-								responseContent = getFile(filename);
-								break;
-					}
+	public byte[] getFile(String filename) {
+		byte[] response = null;
+		try {
+			String path = this.currentPath + "/" + filename;
+			File file = new File(path);
 
-					// ***********************************************
-					// Enviado cabeçalho de resposta com tamanho fixo
-					// ***********************************************
-					byte responseCode = 2;
-					ByteBuffer buffer = this.createResponseHeader(responseCode, commandId, statusCode);
-					bytes = buffer.array();
-					int size = buffer.limit();
-					out.write(bytes, 0, size);
-					out.flush();
-					// ***********************************************
-					// Enviado conteudos do arquivos
-					// ***********************************************
-					
+			FileInputStream inputStream = new FileInputStream(file);
+			response = inputStream.readAllBytes();
+			inputStream.close();
 
-					switch(commandId){
-						case 3: // GETFILESLIST
-								break;
-						case 4: // GETFILE
-							int sizeResponseContent;
-							if(responseContent == null) sizeResponseContent = 0;
-							else  sizeResponseContent = responseContent.length;
-							
-							buffer = ByteBuffer.allocate(4);
-							buffer.order(ByteOrder.BIG_ENDIAN);
-							buffer.putInt(sizeResponseContent);
-							bytes = buffer.array();
-							size = buffer.limit();
-							out.write(bytes, 0, size);
-							out.flush();
-						
-							for(int i = 0; i < sizeResponseContent; i++){
-								System.out.println("Enviou byte: " + responseContent[i]);
-								out.write(responseContent[i]);
+			return response;
+		} catch (Exception e) {
+			return response;
+		}
+	}
+
+	/* metodo executado ao iniciar a thread - start() */
+	@Override
+	public void run() {
+		try {
+
+			System.out.println("args: " + this.currentPath);
+
+			int headerSize = 259;
+
+			while (true) {
+				byte[] bytes = new byte[headerSize];
+				this.in.read(bytes);
+				ByteBuffer header = ByteBuffer.wrap(bytes);
+				header.order(ByteOrder.BIG_ENDIAN);
+
+				// ***********************************
+				// Obtendo cabeçalho de solicitação
+				// ***********************************
+				byte messageType = header.get(0);
+				byte commandId = header.get(1);
+				byte filenameSize = header.get(2);
+				byte[] byteFilename = Arrays.copyOfRange(bytes, 3, filenameSize + 3);
+				String filename = new String(byteFilename);
+
+				byte statusCode = ERROR;
+				byte[] responseContent = null;
+				List<String> getFilesListRespondeContent = null;
+
+				switch (commandId) {
+					case 1: // ADDFILE
+						statusCode = addFile(filename, "conteudo");
+						break;
+					case 2: // DELETE
+						statusCode = removeFile(filename);
+						break;
+					case 3: // GETFILESLIST
+						getFilesListRespondeContent = getFilesList(this.currentPath);
+						break;
+					case 4: // GETFILE
+						responseContent = getFile(filename);
+						break;
+				}
+
+				// ***********************************************
+				// Enviado cabeçalho de resposta com tamanho fixo
+				// ***********************************************
+				byte responseCode = 2;
+				ByteBuffer buffer = this.createResponseHeader(responseCode, commandId, statusCode);
+				bytes = buffer.array();
+				int size = buffer.limit();
+				out.write(bytes, 0, size);
+				out.flush();
+				// ***********************************************
+				// Enviado conteudos do arquivos
+				// ***********************************************
+
+				switch (commandId) {
+					case 3: // GETFILESLIST
+						int listOfFilesSize;
+						if (getFilesListRespondeContent == null)
+							listOfFilesSize = 0;
+						else
+							listOfFilesSize = getFilesListRespondeContent.size();
+
+						buffer = ByteBuffer.allocate(4);
+						buffer.order(ByteOrder.BIG_ENDIAN);
+						buffer.putInt(listOfFilesSize);
+						bytes = buffer.array();
+						size = buffer.limit();
+						out.write(bytes, 0, size);
+						out.flush();
+
+						for (String fileName : getFilesListRespondeContent) {
+							byte[] filenameInBytes = fileName.getBytes();
+							int filenameLength = fileName.length();
+
+							out.write((byte) filenameLength);
+
+							for (int i = 0; i < filenameLength; i++) {
+								System.out.println("Enviou byte: " + filenameInBytes[i]);
+								out.write(filenameInBytes[i]);
 								out.flush();
 							}
+						}
 
-							break;
-					}
+						break;
+					case 4: // GETFILE
+						int sizeResponseContent;
+						if (responseContent == null)
+							sizeResponseContent = 0;
+						else
+							sizeResponseContent = responseContent.length;
 
+						buffer = ByteBuffer.allocate(4);
+						buffer.order(ByteOrder.BIG_ENDIAN);
+						buffer.putInt(sizeResponseContent);
+						bytes = buffer.array();
+						size = buffer.limit();
+						out.write(bytes, 0, size);
+						out.flush();
+
+						for (int i = 0; i < sizeResponseContent; i++) {
+							System.out.println("Enviou byte: " + responseContent[i]);
+							out.write(responseContent[i]);
+							out.flush();
+						}
+
+						break;
 				}
-			} catch (EOFException eofe) {
-					System.out.println("EOF: " + eofe.getMessage());
-			} catch (IOException ioe) {
-					System.out.println("IOE: " + ioe.getMessage());
-			} finally {
-				try {
-					in.close();
-					out.close();
-					clientSocket.close();
-				} catch (IOException ioe) {
-					System.err.println("IOE: " + ioe);
-				}
+
 			}
-			System.out.println("Thread comunicação cliente finalizada.");
+		} catch (EOFException eofe) {
+			System.out.println("EOF: " + eofe.getMessage());
+		} catch (IOException ioe) {
+			System.out.println("IOE: " + ioe.getMessage());
+		} finally {
+			try {
+				in.close();
+				out.close();
+				clientSocket.close();
+			} catch (IOException ioe) {
+				System.err.println("IOE: " + ioe);
+			}
+		}
+		System.out.println("Thread comunicação cliente finalizada.");
 	}
 }
